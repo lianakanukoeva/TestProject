9de862b331dc2e8a3acd9fa2fee7435342238bce
lianakanukoeva <lianakanukoeva@yandex.ru>
2023-02-13 05:07:48 +0300

    edit

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,19 +1,19 @@
 <template>
     <div class="modal__wrapper absolute inset-0 bg-gray-500/75 flex justify-center items-center" v-if="show==true">
         <div class="modal-window max-w-3xl w-3/5 bg-white rounded-md p-5">
-            <Title title="Заказать звонок" />
+            <h2 class="text-gray-900 text-2xl">
+                Заказать звонок
+            </h2>
             <FormComponent @submit.prevent="show==true" />
         </div>
     </div>
 </template>
 
 <script>
-import Title from '@/components/Title'
 import FormComponent from '@/components/forms-components/FormComponent'
 
 export default {
     components: {
-        Title,
         FormComponent
     },
     data: function () {
